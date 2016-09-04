e34c524f1816ac6f7b2f0afe1840ebe6c699f73c
rk <rick.kowaleski@gmail.com>
2016-09-04 12:22:04 -0600

    Base Project Structure

---
+++ src/main/java/com/rk/capstone/MastersCapstoneApplication.java
@@ -0,0 +1,12 @@
+package com.rk.capstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MastersCapstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MastersCapstoneApplication.class, args);
+	}
+}
---
+++ src/main/resources/static/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+    selector: 'app-component',
+    template:`
+        <h2>Front End Place Holder</h2>
+    `
+})
+export class AppComponent {}
