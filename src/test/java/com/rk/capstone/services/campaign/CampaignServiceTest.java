eb3ffdb609f560ce73d088c574a096c560d16800
coboulderer <rick.kowaleski@gmail.com>
2016-09-20 15:44:55 -0600

    Campaign Service Unit tests and test suites

---
+++ src/test/java/com/rk/capstone/services/campaign/CampaignServiceTest.java
@@ -0,0 +1,87 @@
+package com.rk.capstone.services.campaign;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rk.capstone.model.dao.CampaignDao;
+import com.rk.capstone.model.domain.Campaign;
+import com.rk.capstone.model.domain.User;
+import com.rk.capstone.model.services.campaign.CampaignServiceImpl;
+import com.rk.capstone.model.services.campaign.ICampaignService;
+
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+
+/**
+ * Test Class for Campaign Services
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+public class CampaignServiceTest {
+
+    private ICampaignService campaignService;
+    private CampaignDao campaignDao = mock(CampaignDao.class);
+    private Campaign campaignOne;
+    private Campaign campaignTwo;
+    private List<Campaign> campaigns;
+    private User user;
+
+    @Before
+    public void setup() {
+        campaignService = new CampaignServiceImpl(campaignDao);
+        user = new User("firstname", "lastname", "email", "username", "abc123", null);
+        campaignOne = new Campaign("nameOne", "commit", "sol summary", "pending", new Date(), new Date(), 1000, user);
+        campaignTwo = new Campaign("nameTwo", "upside", "another summary", "closed", new Date(), new Date(), 2000, user);
+        campaigns = new ArrayList<>();
+        campaigns.add(campaignOne);
+        campaigns.add(campaignTwo);
+        user.setCampaigns(campaigns);
+    }
+
+    @Test
+    public void testSaveCampaign() {
+        given(this.campaignDao.save(any(Campaign.class))).willReturn(campaignOne);
+        Campaign savedCampaign = campaignService.saveCampaign(campaignOne);
+
+        Assert.assertEquals(campaignOne, savedCampaign);
+    }
+
+    @Test
+    public void testGetOwnedCampaigns() {
+        given(this.campaignDao.findByOwner(any(User.class))).willReturn(campaigns);
+        List<Campaign> foundCampaigns = campaignService.getOwnedCampaigns(user);
+
+        Assert.assertEquals(campaigns, foundCampaigns);
+    }
+
+    @Test
+    public void testGetOwnedCampaignsNoneFound() {
+        given(this.campaignDao.findByOwner(any(User.class))).willReturn(null);
+        List<Campaign> foundCampaigns = campaignService.getOwnedCampaigns(user);
+
+        Assert.assertNull(foundCampaigns);
+    }
+
+    @Test
+    public void testGetCampaignById() {
+        given(this.campaignDao.findByCampaignId(any(Long.class))).willReturn(campaignOne);
+        Campaign foundCampaign = campaignService.getCampaignById(1L);
+
+        Assert.assertEquals(campaignOne, foundCampaign);
+    }
+
+    @Test
+    public void testGetCampaignByIdNotFound() {
+        given(this.campaignDao.findByCampaignId(any(Long.class))).willReturn(null);
+        Campaign foundCampaign = campaignService.getCampaignById(1L);
+
+        Assert.assertNull(foundCampaign);
+    }
+}
