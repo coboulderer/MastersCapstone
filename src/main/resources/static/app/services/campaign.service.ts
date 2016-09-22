f0146e43b8aff385a80b2a2363a61bb3de83185f
coboulderer <rick.kowaleski@gmail.com>
2016-09-22 15:50:37 -0600

    Campaign Service stubbing

---
+++ src/main/resources/static/app/services/campaign.service.ts
@@ -0,0 +1,28 @@
+import {Injectable} from "@angular/core";
+import {Http, Headers} from "@angular/http";
+
+@Injectable()
+export class CampaignService {
+
+    constructor(private http:Http){}
+
+    getAllUserCampaigns() {
+        // TODO - Implement
+    }
+
+    getCampaign() {
+        // TODO - Implement
+    }
+
+    createNewCampaign() {
+        // TODO - Implement
+    }
+
+    updateCampaign() {
+        // TODO - Implement
+    }
+
+    deleteCampaign() {
+        // TODO - Implement
+    }
+}
