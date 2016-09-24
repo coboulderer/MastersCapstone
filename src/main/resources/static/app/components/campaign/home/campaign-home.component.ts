a756ab6fcc31cb8b29f278f7a33a93b2e0164665
rk <rick.kowaleski@gmail.com>
2016-09-24 12:16:52 -0600

    Clean up and add missing fields to new campaign form

--- src/main/resources/static/app/components/campaign/home/campaign-home.component.ts
+++ src/main/resources/static/app/components/campaign/home/campaign-home.component.ts
@@ -12,24 +12,39 @@ import {Campaign} from "../../../model/campaign";
 export class CampaignHome implements OnInit{
 
     private currentCampaign: Campaign;
-    private customerCampaigns: Campaign[];
+    private allUserCampaigns: Campaign[];
 
-    constructor(private campaignService: CampaignService){}
+    constructor(private campaignService: CampaignService){
+        this.allUserCampaigns = [];
+    }
 
     ngOnInit():void {
-        this.customerCampaigns = [];
-        // TODO - Get All User Campaigns
-        // TODO - Display first one
+        console.log("Init CampaignHome Component and checking for existing campaigns");
+        this.loadCampaigns();
     }
 
     campaignCreated(campaign: Campaign) {
-        console.log("CampaignHome.createNewCampaign() function called");
+        console.log("CampaignHome.campaignCreated() function called");
         this.currentCampaign = campaign;
-        //this.customerCampaigns.push(campaign);
+        this.allUserCampaigns.push(campaign);
     }
 
     addTask() {
         console.log("CampaignHome.addTask() function called");
         // TODO - Implementation
+    }
+
+    private loadCampaigns() {
+        this.campaignService.getAllUserCampaigns().subscribe(campaigns => {
+                console.log("Found User Campaigns");
+                if (campaigns.length > 0) {
+                    this.allUserCampaigns = campaigns;
+                    this.currentCampaign = this.allUserCampaigns[0];
+                }
+            },
+            error => {
+                console.log("Error Caught in CampaignHome.loadCampaigns()");
+                console.log("Error Message:\n" + error);
+            });
     }
 }
