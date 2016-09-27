013d60a4d2d056abb77d44375f7faa245c5ccc21
coboulderer <rick.kowaleski@gmail.com>
2016-09-27 12:04:57 -0600

    Create front end Task

---
+++ src/main/resources/static/app/model/task.ts
@@ -0,0 +1,8 @@
+export class Task {
+    public taskId: number;
+    public campaignId: number;
+    public dueDate: number;
+    public description: string;
+    public assignedTo: string;
+    public isCompleted: boolean;
+}
