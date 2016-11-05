b9cd093d84190880e13c7bcd35510c39892eeb45
coboulderer <rick.kowaleski@gmail.com>
2016-11-05 11:50:41 -0600

    Add logging to backend rest controllers

--- src/main/java/com/rk/capstone/controllers/secure/campaign/CampaignController.java
+++ src/main/java/com/rk/capstone/controllers/secure/campaign/CampaignController.java
@@ -2,6 +2,8 @@ package com.rk.capstone.controllers.secure.campaign;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +27,8 @@ public class CampaignController {
     private IUserService userService;
     private ICampaignService campaignService;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public CampaignController(IUserService userService, ICampaignService campaignService) {
         this.userService = userService;
         this.campaignService = campaignService;
@@ -35,11 +39,16 @@ public class CampaignController {
                                                       @RequestBody Campaign campaign) {
         ResponseEntity<Campaign> response;
         User user = userService.findByUserName(userName);
+        logger.info("Attempting to create a new campaign");
         if (campaign == null || campaign.getCampaignId() != null) {
+            logger.error("Campaign creation failed - the provided campaign is null or already has" +
+                    " an id");
             response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } else if (user == null) {
+            logger.error("Null users cannot create a campaign");
             response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } else {
+            logger.info("Creating New Campaign");
             campaign.setOwner(user);
             campaign = campaignService.saveCampaign(campaign);
             response = ResponseEntity.status(HttpStatus.CREATED).body(campaign);
@@ -51,14 +60,19 @@ public class CampaignController {
     public ResponseEntity<Campaign> updateCampaign(@PathVariable Long campaignId,
                                                    @RequestBody Campaign campaign) {
         ResponseEntity<Campaign> response;
+        logger.info("Attempting to update the provided campaign");
         if (campaignId == null || campaign == null || campaign.getCampaignId() == null ||
                 !campaignId.equals(campaign.getCampaignId())) {
+            logger.error("Cannot update null objects");
             response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } else {
             Campaign foundCampaign = campaignService.getCampaignById(campaignId);
             if (foundCampaign == null) {
+                logger.error("The provided campaign was not found");
+                logger.error(campaign.toString());
                 response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(campaign);
             } else {
+                logger.info("Updating");
                 campaign.setOwner(foundCampaign.getOwner());
                 campaign = campaignService.saveCampaign(campaign);
                 response = ResponseEntity.status(HttpStatus.OK).body(campaign);
@@ -70,11 +84,16 @@ public class CampaignController {
     @RequestMapping(value = "/{campaignId}", method = RequestMethod.DELETE)
     public ResponseEntity<String> deleteCampaign(@PathVariable Long campaignId) {
         ResponseEntity<String> response;
+        logger.info("Attempting to delete the provided campaign");
         if (campaignId == null) {
+            logger.error("Cannot delete a campaign with a null id");
             response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } else if (campaignService.getCampaignById(campaignId) == null) {
+            logger.error("Could not find the desired campaignId: " + campaignId);
+            logger.error("Delete failed");
             response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("Campaign Not Found");
         } else {
+            logger.info("Deleting campaign with id = " + campaignId);
             campaignService.deleteCampaignById(campaignId);
             response = ResponseEntity.status(HttpStatus.OK).body("Campaign Deleted - id = " +
                     campaignId);
@@ -85,13 +104,17 @@ public class CampaignController {
     @RequestMapping(value = "/{campaignId}", method = RequestMethod.GET)
     public ResponseEntity<Campaign> getCampaign(@PathVariable Long campaignId) {
         ResponseEntity<Campaign> response;
+        logger.info("Attempting to retrieve specified campaignId: " + campaignId);
         if (campaignId == null) {
+            logger.error("Failed - cannot get a null campaignId");
             response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } else {
             Campaign campaign = campaignService.getCampaignById(campaignId);
             if (campaign == null) {
+                logger.error("The requested campaignId: " + campaignId + " was not found");
                 response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
             } else {
+                logger.info("Campaign found - returning campaignId: " + campaignId);
                 response = ResponseEntity.status(HttpStatus.OK).body(campaign);
             }
         }
@@ -102,13 +125,18 @@ public class CampaignController {
     public ResponseEntity<List<Campaign>> getAllUserCampaigns(@PathVariable String userName) {
         ResponseEntity<List<Campaign>> response;
         User user = userService.findByUserName(userName);
+        logger.info("Attempting to retrieve all user campaigns");
         if (userName == null || userName.isEmpty()) {
+            logger.error("Cannot get campaigns of a null or empty username");
             response = ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
         } else if (user == null) {
+            logger.error("The provided userName: " + userName + " was not found");
+            logger.error("Campaign retrieval failed");
             response = ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
         } else {
             List<Campaign> campaigns = campaignService.getOwnedCampaigns(user);
             response = ResponseEntity.status(HttpStatus.OK).body(campaigns);
+            logger.info("Returning all user campaigns, " + campaigns.size() + " campaigns found");
         }
         return response;
     }
--- src/main/java/com/rk/capstone/controllers/secure/customer/company/CustomerCompanyController.java
+++ src/main/java/com/rk/capstone/controllers/secure/customer/company/CustomerCompanyController.java
@@ -2,6 +2,8 @@ package com.rk.capstone.controllers.secure.customer.company;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +24,8 @@ public class CustomerCompanyController {
 
     private ICustomerCompanyService customerCompanyService;
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
     public CustomerCompanyController(ICustomerCompanyService customerCompanyService) {
         this.customerCompanyService = customerCompanyService;
     }
@@ -30,9 +34,12 @@ public class CustomerCompanyController {
     public ResponseEntity<List<CustomerCompany>> getAllCustomerCompanies() {
         ResponseEntity<List<CustomerCompany>> response;
         List<CustomerCompany> customerCompanies = customerCompanyService.getAllCustomerCompanies();
+        logger.info("Attempting to get all customer companies");
         if (customerCompanies == null || customerCompanies.isEmpty()) {
+            logger.info("No customer companies found - returning no content");
             response = ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
         } else {
+            logger.info("Returning " + customerCompanies.size() + " customer companies");
             response = ResponseEntity.status(HttpStatus.OK).body(customerCompanies);
         }
         return response;
@@ -43,9 +50,12 @@ public class CustomerCompanyController {
             @RequestBody CustomerCompany customerCompany) {
         ResponseEntity<CustomerCompany> response;
         customerCompany = customerCompanyService.saveCustomerCompany(customerCompany);
+        logger.info("Attempting to create a new customer company");
         if (customerCompany == null || customerCompany.getId() == 0L) {
+            logger.error("Cannot create a null or preexisting customer company");
             response = ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
         } else {
+            logger.info("Creating new customer company");
             response = ResponseEntity.status(HttpStatus.CREATED).body(customerCompany);
         }
         return response;
@@ -55,10 +65,13 @@ public class CustomerCompanyController {
     public ResponseEntity<String> deleteCustomerCompany(@PathVariable long id) {
         ResponseEntity<String> response;
         CustomerCompany customerCompany = customerCompanyService.getCustomerCompanyById(id);
+        logger.info("Attempting to delete customer company with id = " + id);
         if (customerCompany == null) {
+            logger.error("Customer company with id = " + id + " not found, delete failed");
             response = ResponseEntity.status(HttpStatus.NOT_FOUND).body("The Customer Company you " +
                     "requested was not found.");
         } else {
+            logger.info("Found customer company with id = " + id + ", deleting");
             customerCompanyService.deleteCustomerCompanyById(id);
             response = ResponseEntity.status(HttpStatus.OK).body("Customer Company Deleted");
         }
