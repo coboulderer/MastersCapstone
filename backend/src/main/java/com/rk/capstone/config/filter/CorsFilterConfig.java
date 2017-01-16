53d139f60080ea8ab5f2a387e326744bd6598bfb
coboulderer <rick.kowaleski@gmail.com>
2017-01-16 14:48:27 -0700

    Major Refactor: Split Front and Backend into separate projects Add CorsFilter to backend to support Front end running as separate process on same host

---
+++ backend/src/main/java/com/rk/capstone/config/filter/CorsFilterConfig.java
@@ -0,0 +1,29 @@
+package com.rk.capstone.config.filter;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Cors Filter
+ */
+@Configuration
+public class CorsFilterConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowCredentials(true);
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("GET");
+        configuration.addAllowedMethod("PUT");
+        configuration.addAllowedMethod("POST");
+        configuration.addAllowedMethod("DELETE");
+        source.registerCorsConfiguration("/**", configuration);
+        return new CorsFilter(source);
+    }
+}
